fc11df835c61b68bf134e0a8d290ad70e751037e
Madura Herath <maduraherath8@gmail.com>
2017-05-29 03:03:32 +0530

    error latlng cannot be null

--- index.android.js
+++ index.android.js
@@ -9,13 +9,74 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  View
+  View,
+  Dimensions
 } from 'react-native';
-
+const {width,height}=Dimensions.get('window')
+const ASPECT_RATIO = width / height;
+const LATITUDE = 51.023033;
+const LONGITUDE = 10.3621663;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 export default class Shop extends Component {
+    
+    constructor(props){
+        super(props);
 
+        this.state={
+            initialPosition:{
+            latitude:0,
+            longitude:0,
+            latitudeDelta:LATITUDE_DELTA,
+            longitudeDelta:LONGITUDE_DELTA
 
+            },
+            makerPosition:{
+                latitude:37.78825,
+                longtitude:-122.4324,
+            }
+        }
+    }
+    watchID: ?number = null
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+          
+          console.log(position);
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
+        var initialRegion = {
+            latitude:lat,
+            longitude:long,
+            latitudeDelta:LATITUDE_DELTA,
+            longitudeDelta:LONGITUDE_DELTA
+        }
+        this.setState({initialPosition: initialRegion})
+        this.setState({makerPosition: initialRegion})
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+        
+     var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+              var lastRegion = {
+            latitude:lat,
+            longitude:long,
+            latitudeDelta:LATITUDE_DELTA,
+            longitudeDelta:LONGITUDE_DELTA
+           
+            
+        }
+        this.setState({initialPosition:lastRegion})
+        this.setState({makerPosition:lastRegion})
+    })
+  }
+componentWillUnmount(){
+    navigator.geolocation.clearWatch(this.watchID)
+}
 
 render() {
   return ( 
@@ -32,12 +93,19 @@ render() {
                 </Header>
  <View style ={styles.container}>
         <MapView
+        showsUserLocation={true}
           style={styles.map}
-          initialRegion={{    latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,}}>
+         region ={ this.state.initialPosition }>
+          <MapView.Marker region={this.state.markerPosition}>
+              <View style={styles.radius}>
+                  <View style={styles.marker}>
+                      </View>
+                  </View>
+              </MapView.Marker>
         </MapView>
+      </View>
+      <View>
+
       </View>
  </Container>
 
@@ -46,7 +114,27 @@ render() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  radius:{
+height:50,
+width:50,
+borderRadius:50/2,
+overflow:'hidden',
+backgroundColor:'rgba(10,122,255,0.1)',
+borderWidth:1,
+borderColor:'rgba(0,122,255,0.3)',
+alignItems: 'center',
+justifyContent:'center'
+  }, 
+  marker:{
+height:20,
+width:20,
+borderWidth:3,
+overflow:'hidden',
+backgroundColor:'red',
+borderColor:'white',
+borderRadius:20/2
+  },
+    container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
